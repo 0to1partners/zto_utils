595ca8f2c7f47ebc603eabebf9a220de331ba740
ztop-ytkim <85145156+ztop-ytkim@users.noreply.github.com>
2021-06-07 11:24:29 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ setup(
     name = 'zto_utils',
     version = '0.1.0',
     url = 'https://github.com/0to1partners/zto_utils.git',
-    author = 'aru'
-    author_email = 'ytkim@0to1partners.com'
+    author = 'aru',
+    author_email = 'ytkim@0to1partners.com',
     packages = ['zto_utils'],
     zip_safe = False,
     install_requires = []
