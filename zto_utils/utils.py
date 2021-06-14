2dfaa092b3336e48aa92353293d650b507697ce4
ytkim <ytkim@0to1partners.com>
2021-06-14 14:54:38 +0900

    add_timer

--- zto_utils/utils.py
+++ zto_utils/utils.py
@@ -1,6 +1,9 @@
+#%%
 import logging
 import os
-import datetime
+from datetime import datetime
+from time import time
+from functools import wraps
 
 
 class SingletonMeta(type) :
@@ -10,6 +13,41 @@ class SingletonMeta(type) :
         except AttributeError :
             cls.__instance = super(SingletonMeta, cls).__call__(*args, **kwargs)
             return cls.__instance
+
+
+
+# def time_elapsed(func, *args, **kwargs) :
+#     @wraps(func)
+#     def wrapper(func, *args, **kwargs) :
+
+#         time_start = datetime.now()
+#         ret = func(*args, **kwargs)
+#         time_elapsed = datetime.now() - time_start
+
+#         CustomLogger().info(f'{func} : time elpased ')
+#         return ret
+    
+#     return wrapper
+
+
+class TimeDecorator() :
+    def __init__(self, f) :
+        self.func = f
+
+    def __call__(self, *args, **kwargs) :
+        time_start = time.time()
+        ret = self.func(*args, **kwargs)
+        time_elapsed = round(time.time() - time_start, 3)
+
+        msg = f'{self.func.__name__}'
+        if len(args) > 0 :
+            msg = msg + f'{args}'
+        if len(kwargs) > 0 :
+            msg = msg + f'{kwargs}'
+        msg = f'Function {msg} : {time_elapsed}s elapsed '
+        CustomLogger().info(msg)
+        return ret
+        
 
 
 
@@ -24,7 +62,7 @@ class CustomLogger(metaclass = SingletonMeta) :
             '[%(asctime)s][%(levelname)s][%(filename)s:%(lineno)s] > %(message)s'
             )
 
-        now = datetime.datetime.now()
+        now = datetime.now()
 
         dirname = './log'
         if not os.path.exists(dirname) :
@@ -57,3 +95,4 @@ class CustomLogger(metaclass = SingletonMeta) :
 
     def critical(self, *args, **kwargs) :
         self._logger.critical(*args, *kwargs)
+
